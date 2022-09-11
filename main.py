f05e42be9a8610186010be6c5b4883d79de16773
Swannhs <princeapurbask@gmail.com>
2022-09-11 22:30:28 +0600

    Initialize commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+import random
+
+# for z in range(1, 500):
+#     x = random.randint(0, 500)
+#     y = random.randint(0, 500)
+#     pyautogui.move(x, y)
+#
+#     localTime = time.localtime()
+#     result = time.strftime("%I:%M:%S %p", localTime)
+#
+#     print('Move at', str(result) + str(x) + ' ' + str(y))
+#     time.sleep(2)
+
+pyautogui.press('f1')
